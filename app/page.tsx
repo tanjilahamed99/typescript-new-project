2c695651bdc9e5d8b66f414db1a2ecf60727989f
tanjilahamed99 <tanjil113355@gmail.com>
2025-04-06 11:38:37 +0600

    banner , footer and show data component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import Footer from "@/components/Footer";
 import Navbar from "../components/Navbar";
+import Banner from "@/components/Banner";
+import ShowData from "@/components/ShowData";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Banner />
+
+      <div>
+        <ShowData />
+      </div>
+
+      <Footer />
     </div>
   );
 }
---
+++ components/ShowData.tsx
@@ -0,0 +1,7 @@
+import data from "../public/data/people.json";
+
+const ShowData = () => {
+  return <div></div>;
+};
+
+export default ShowData;
